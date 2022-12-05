a38e37ae8deb0e1d53e673d825dba2e087312627
Susmitha Poshanna Dhuta <primesoft_sdhuta@outlook.com>
2022-12-05 21:00:54 +0530

    Prove Task4, Task5

---
+++ ProveTasks/ProveKT4cs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProveTasks
+{
+    public abstract class Animal
+    {
+        public string Name; // name property
+        public void SetName(string name) // SetName method which takes string as parameter
+        {
+            Name = name;
+        }
+       public String GetName() // GetName method which returns string Name
+        {
+            return Name;
+
+        }
+        public abstract void Eat(); // abstract method which needs further Implementation
+    }
+
+    public class Dog : Animal //Dog class which is Inheriting from Animal class
+    {
+        public override void Eat() //overriding Eat method
+        {
+            Console.WriteLine("Eating");
+        }
+    }
+
+    public class ProveKT4
+    {
+        public static void Main(string[] args) //Main method-entry point of program
+        {
+            Dog dog = new Dog(); // Instantiating Dog class
+            dog.SetName(Console.ReadLine()); // Reading Input from user
+            
+            Console.WriteLine(dog.GetName()); // Printing the data
+            dog.Eat();
+        }
+    }
+}
---
+++ ProveTasks/ProveKT5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProveTasks
+{
+    class ProveKT5
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 10; 
+            int[] numbers = new int[sum]; // defining number of interger elements in array
+            for (int i = 0; i < sum; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine()); // storing the user inputs in array called numbers
+            }
+            Console.WriteLine("The List of even Numbers from Integer Array:");
+
+            for (int i=0; i < sum; i++) //for loop for iterations
+            {
+                if (numbers[i] %2 == 0)  //Logic for sorting the even numbers from array list
+                {                 
+                  Console.WriteLine("{0}", numbers[i]); // printing the even numbers after sorting
+                }
+            }
+        }
+    }
+}
