f343130441113584461feb6724ece4ac1a8e9b63
Susmitha Poshanna Dhuta <primesoft_sdhuta@outlook.com>
2022-12-06 19:18:47 +0530

    Prove Task6, Task7

---
+++ ProveTasks/ProveKT6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProveTasks
+{
+    class ProveKT6
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text:");
+            string text = Console.ReadLine();
+
+            if (IsNumber(text))
+            {
+                Console.WriteLine("Is Number");
+
+            }
+            else
+            {
+                Console.WriteLine("Is Not a Number");
+            }
+        }
+
+
+        public static bool IsNumber(string text)
+       
+           {
+
+            for (int i = 0; i<text.Length; i++)
+            {
+                if (char.IsDigit(text[i]) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+     }
+  }
+
