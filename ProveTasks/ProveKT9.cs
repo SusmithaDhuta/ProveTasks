9fa17b84a83f48eae43913d790db0ed9e8b83ff8
Susmitha Poshanna Dhuta <primesoft_sdhuta@outlook.com>
2022-12-07 18:55:25 +0530

    Prove Task8, Task9

---
+++ ProveTasks/ProveKT9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProveTasks
+{
+    class ProveKT9
+    {
+        public static void Main(string[] args)
+        {
+            int Number1 = int.Parse(Console.ReadLine());
+            int Number2 = int.Parse(Console.ReadLine());
+            int total=0;
+
+            for(int i=0;i<Number2;i++)
+            {
+                total = total+ Number1;
+            }
+            Console.WriteLine("The Multiplication of Two Numbers:{0}", total);
+        }
+    }
+}
