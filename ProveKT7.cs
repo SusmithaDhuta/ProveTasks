11e889da5eba42641b95c865bc2955228d47b0d2
SusmithaDhuta <119501815+SusmithaDhuta@users.noreply.github.com>
2022-12-08 11:32:52 +0530

    Add files via upload

---
+++ ProveKT7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ProveTasks
+{
+    class ProveKT7
+    {
+        public static void Main(string[] args)
+        {
+            string inputFileName = "input.txt"; 
+            string outputFileName = "output.txt";
+
+            string contents = File.ReadAllText(inputFileName);
+            contents = contents.ToUpper();
+
+            File.WriteAllText(outputFileName, contents);
+        }
+    }
+}
---
+++ ProveKT8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace ProveTasks
+{
+    class ProveKT8
+    {
+        public static void Main(string[] args)
+        {
+            int total = 3;
+            ArrayList arrayList = new ArrayList();
+
+            for (int i = 0; i < total; i++)
+            {
+
+                string name = Console.ReadLine();
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                arrayList.Add(new Person() { Name = name, Age = age });
+            }
+
+            Console.WriteLine("List of Person Name and Age in ArrayList :");
+            foreach (Person p in arrayList)
+            {
+              Console.WriteLine ( p.ToString());
+            }           
+        }
+          
+    }
+        public class Person
+        {
+           public string Name;
+            public int Age;
+            public override string ToString()
+            {
+                return Name + ":" + Age;
+            }
+
+        }   
+}
